2434aac38592466e5607e52e64df59612f7df544
changle <changle@xmylsw.com>
2018-05-19 12:29:56 +0800

    修改过滤器中过滤的方法,permission中增加method条件判断

--- src/main/java/com/example/blog/config/security/auth/CustomizedAuthFilterInvocationSecurityMetadataSource.java
+++ src/main/java/com/example/blog/config/security/auth/CustomizedAuthFilterInvocationSecurityMetadataSource.java
@@ -9,6 +9,7 @@ import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
@@ -23,9 +24,9 @@ import java.util.*;
 public class CustomizedAuthFilterInvocationSecurityMetadataSource implements FilterInvocationSecurityMetadataSource {
 
     /**
-     * 保存所有权限与url关系的Map
+     * 保存所有权限的Map,使用name作为key,Permission作为值
      */
-    private Map<String, String> authorityToUrlsMap;
+    private Map<String, Permission> permissionsMap;
 
     @Autowired
     private PermissionRepository permissionRepository;
@@ -35,9 +36,9 @@ public class CustomizedAuthFilterInvocationSecurityMetadataSource implements Fil
      * Permission中url值与Authority一一对应
      */
     private void loadResourceDefine() {
-        authorityToUrlsMap = new HashMap<>();
-        List<Permission> permissionList = permissionRepository.findAll();
-        permissionList.forEach(permission -> authorityToUrlsMap.put(permission.getAuthority(), permission.getUrl()));
+        this.permissionsMap = new HashMap<>();
+        Collection<Permission> permissionCollection = permissionRepository.findAll();
+        permissionCollection.forEach(permission -> this.permissionsMap.put(permission.getAuthority(), permission));
     }
 
     /**
@@ -48,13 +49,15 @@ public class CustomizedAuthFilterInvocationSecurityMetadataSource implements Fil
     public Collection<ConfigAttribute> getAttributes(Object o) throws IllegalArgumentException {
         // 保证所有权限与资源是实时刷新的
         loadResourceDefine();
-        HttpServletRequest request = ((FilterInvocation) o).getHttpRequest();
-        Collection<ConfigAttribute> configAttributeCollection = new ArrayList<>();
-        authorityToUrlsMap.forEach((authority, urls) -> {
-            for (String url : urls.split(",")) {
-                if (new AntPathRequestMatcher(url).matches(request)) {
-                    configAttributeCollection.add(new SecurityConfig(authority));
-                    break;
+        HttpServletRequest httpServletRequest = ((FilterInvocation) o).getHttpRequest();
+        Collection<ConfigAttribute> configAttributeCollection = new HashSet<>();
+        this.permissionsMap.forEach((authority, permission) -> {
+            if (methodMatches(httpServletRequest, permission)) {
+                for (String url : permission.getUrl().split(",")) {
+                    if (new AntPathRequestMatcher(url).matches(httpServletRequest)) {
+                        configAttributeCollection.add(new SecurityConfig(authority));
+                        break;
+                    }
                 }
             }
         });
@@ -67,14 +70,28 @@ public class CustomizedAuthFilterInvocationSecurityMetadataSource implements Fil
     @Override
     public Collection<ConfigAttribute> getAllConfigAttributes() {
         loadResourceDefine();
-        Collection<ConfigAttribute> configAttributeCollection = new ArrayList<>();
-        authorityToUrlsMap.forEach((authority, urls) -> configAttributeCollection.add(new SecurityConfig(authority)));
+        Collection<ConfigAttribute> configAttributeCollection = new HashSet<>();
+        permissionsMap.forEach((authority, permission) -> configAttributeCollection.add(new SecurityConfig(authority)));
         return configAttributeCollection;
     }
 
     @Override
     public boolean supports(Class<?> aClass) {
         return FilterInvocation.class.isAssignableFrom(aClass);
+    }
+
+    private boolean methodMatches(HttpServletRequest httpServletRequest, Permission permission) {
+        if (!StringUtils.hasText(permission.getMethod()) || permission.getMethod().equals("*")) {
+            return true;
+        }
+        String[] methods = permission.getMethod().split(",");
+        for (String method : methods) {
+            method = method.replaceAll("\\s", "").toUpperCase();
+            if (httpServletRequest.getMethod().equals(method)) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
